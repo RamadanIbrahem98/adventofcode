716164c13f6778b492d4a90cdfc7d51b13fbc9c9
Ramadan Ibrahim <ramadan.ibrahem98@eng-st.cu.edu.eg>
2023-12-09 00:48:06 +0200

    2023: day08 refactor

--- 2023/day08/main.py
+++ 2023/day08/main.py
@@ -1,31 +1,32 @@
 from collections import defaultdict
+import math
 
 directionsMap = defaultdict(list)
 directions_mapping = {"R": "1", "L": "0"}
-
-def gcd(a, b):
-  while b:
-    a, b = b, a%b
-  return a
-
-def LCM(nums):
-  lcm = nums[0]
-  for i in nums[1:]:
-    lcm = lcm*i//gcd(lcm, i)
-
-  return lcm
 
 def read_input(file: str = "input.txt"):
   global sequence, directions
   with open(file) as file:
     sequence, *[directions] = file.read().split("\n\n")
 
-def partOne():
-  global sequence
   sequence = sequence.translate(str.maketrans(directions_mapping))
   for el in directions.strip().split("\n"):
     x, y = el.split(" = ")
     directionsMap[x.strip()] = [e.strip() for e in y[1:-1].split(", ")]
+
+def aToZSteps(end):
+  global sequence
+  steps = 0
+  while True:
+    for i in sequence:
+      direction = directionsMap[end][int(i)]
+      end = direction
+      steps += 1
+      if (end[-1] == 'Z'):
+        return steps
+
+def partOne():
+  global sequence
 
   end = 'AAA'
   steps = 0
@@ -40,27 +41,11 @@ def partOne():
 
 def partTwo():
   global sequence
-  sequence = sequence.translate(str.maketrans(directions_mapping))
-  for el in directions.strip().split("\n"):
-    x, y = el.split(" = ")
-    directionsMap[x.strip()] = [e.strip() for e in y[1:-1].split(", ")]
-
+  
   ends = [x for x in directionsMap.keys() if x[-1] == 'A']
-  steps = 0
-  LCMs = [0 for i in range(len(ends))]
-  foundZ = [1 for i in range(len(ends))]
+  LCMs = [aToZSteps(end) for end in ends]
 
-  while True:
-    for i in sequence:
-      for j, x in enumerate(ends):
-        if (foundZ[j] == 0):
-          continue
-        ends[j] = directionsMap[x][int(i)]
-        LCMs[j] = (LCMs[j] + 1) if foundZ[j] == 1 else LCMs[j]
-        foundZ[j] = 0 if ends[j][-1] == 'Z' else 1
-      steps += 1
-      if 1 not in foundZ:
-        return LCM(LCMs)
+  return math.lcm(*LCMs)
 
 if __name__ == "__main__":
   read_input()
